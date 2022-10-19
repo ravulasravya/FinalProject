b8afa0571bac37fe000e80256130a25f3c5fd6a7
ravulasravya <53582963+ravulasravya@users.noreply.github.com>
2022-10-19 00:14:27 -0500

    Add files via upload

---
+++ src/main/java/com/uofa/ws_final_wheelsAndMore/WsFinalWheelsAndMoreApplication.java
@@ -0,0 +1,13 @@
+package com.uofa.ws_final_wheelsAndMore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsFinalWheelsAndMoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsFinalWheelsAndMoreApplication.class, args);
+	}
+
+}
